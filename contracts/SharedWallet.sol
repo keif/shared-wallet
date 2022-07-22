2761d4adfae598e0de36ab72955bb0b0f6e114d7
Keith Baker <github@baker.af>
2022-07-22 01:00:21 -0400

    Securing the smart contract

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -3,9 +3,23 @@
 pragma solidity 0.8.15;
 
 contract SharedWallet {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed");
+        _;
+    }
+
     function receiveMoney() public payable {}
 
-    function withdrawMoney(address payable _to, uint256 _amount) public {
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        onlyOwner
+    {
         _to.transfer(_amount);
     }
 
